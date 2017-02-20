3582a04d9477b65b75b63f360b4b886035918195
Housame <housame.oueslati@hotmail.com>
2017-02-20 09:55:34 +0100

    add rsp folder that contains all rsp pictures

--- js/angular-mod.js
+++ js/angular-mod.js
@@ -1,7 +1,12 @@
 /**
  * Created by Housam on 2017-02-15.
  */
+/*Angular method of calling parts*/
+
+/*Angular module*/
 var app = angular.module("myApp", ["ngRoute"]);
+
+/*Angular controller*/
 app.config(function($routeProvider) {
     $routeProvider
         .when("/tictactoe", {
--- js/stensax.js
+++ js/stensax.js
@@ -3,13 +3,20 @@
  */
 var player = 0;
 var CPU = 0;
-var winnerIMG="img/winner.jpg";
-var loserIMG="img/loser.jpg";
-var noneIMG="img/tryagain.jpg";
+var winnerIMG="img/rsp/winner.jpg";
+var loserIMG="img/rsp/loser.jpg";
+var noneIMG="img/rsp/tryagain.jpg";
+/* Start Game with the choice that user made*/
 function StartGame(choice){
     document.getElementById("gameBoxx").innerHTML = "";
     Game(choice);
 }
+/*get a random choice, compare with users choice, fill the table with
+* 1st:th with "You" & "CPU"
+* 2nd:the maiden choices (pictures)
+* 3rd:Winning or loosing results (pictures)
+* Then it initiale player and CPU.
+* */
 function Game(choice){
     var userChoice = choice;
     var computerChoice = Math.random();
@@ -22,33 +29,36 @@ function Game(choice){
     compare(userChoice,computerChoice);
     UchoiceIMGURL(userChoice);
     CPUchoiceIMGURL(computerChoice);
-
     ResultTableFill(player,CPU);
     player=0;
     CPU=0;
 
 }
+/* FIll the first row in the result table*/
 function FillTH(){
     document.getElementById("you").innerHTML = "<h3>You</h3>";
     document.getElementById("aI").innerHTML = "<h3>CPU</h3>";
 }
+/* Fill the user choice in [second row, first column]*/
 function UchoiceIMGURL (userChoice){
     var playerChoiceTD = document.getElementById("playerChoice");
     if (userChoice.toUpperCase()==="ROCK")
-    {playerChoiceTD.innerHTML="<img src='img/rock.jpg'>"}
+    {playerChoiceTD.innerHTML="<img src='img/rsp/rock.jpg'>"}
     else if (userChoice.toUpperCase()==="PAPER")
-    { playerChoiceTD.innerHTML="<img src='img/paper.jpg'>"}
-    else playerChoiceTD.innerHTML="<img src='img/scissor.jpg'>";
+    { playerChoiceTD.innerHTML="<img src='img/rsp/paper.jpg'>"}
+    else playerChoiceTD.innerHTML="<img src='img/rsp/scissor.jpg'>";
 
 }
+/* Fill the computer choice in [second row, second column]*/
 function CPUchoiceIMGURL (computerChoice){
     var cpuChoiceTD = document.getElementById("cpuChoice");
     if (computerChoice.toUpperCase()==="ROCK")
-    {cpuChoiceTD.innerHTML="<img src='img/rock.jpg'>"}
+    {cpuChoiceTD.innerHTML="<img src='img/rsp/rock.jpg'>"}
     else if (computerChoice.toUpperCase()==="PAPER")
-    { cpuChoiceTD.innerHTML="<img src='img/paper.jpg'>"}
-    else cpuChoiceTD.innerHTML="<img src='img/scissor.jpg'>";
+    { cpuChoiceTD.innerHTML="<img src='img/rsp/paper.jpg'>"}
+    else cpuChoiceTD.innerHTML="<img src='img/rsp/scissor.jpg'>";
 }
+/* Fill the results of user and computer */
 function ResultTableFill(player,CPU){
     var playerTD = document.getElementById("playerResult");
     var cpuTD = document.getElementById("cpuResult");
@@ -68,6 +78,7 @@ function ResultTableFill(player,CPU){
         cpuTD.innerHTML="<img src='"+noneIMG+"')</img>";
     }
 }
+/* Compare the two choices and give a result in form of 0 and 1*/
 function compare(choice1,choice2) {
     choice1=choice1.toUpperCase();
     choice2=choice2.toUpperCase();
@@ -108,7 +119,7 @@ function compare(choice1,choice2) {
             }
     }
 }
-
+/*Reset the game*/
 function Reset(){
     window.location.href="#/";
     window.location.href="#/sten";
